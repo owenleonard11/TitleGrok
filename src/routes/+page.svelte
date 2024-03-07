d427bb7a6787ca0a58a4c52b8fbb1969193ee365
Owen Leonard <owenleonard11@gmail.com>
2024-03-07 14:36:58 -0500

    Add import accordion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,59 @@
 <script lang="ts">
     import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+    import { FileDropzone } from '@skeletonlabs/skeleton';
 </script>
 
-<Accordion class="px-8 pt-8 text-xl">
+<Accordion class="px-8 pb-4 text-xl">
     <AccordionItem open>
         <svelte:fragment slot="summary"><h2 class="h2">Step 1: Import</h2></svelte:fragment>
         <svelte:fragment slot="content">
-            Content Here!
+            <Accordion autocollapse>
+                <AccordionItem>
+                    <svelte:fragment slot="iconClosed"></svelte:fragment>
+	                <svelte:fragment slot="iconOpen"></svelte:fragment>
+                    <svelte:fragment slot="lead">(link icon)</svelte:fragment>
+                    <svelte:fragment slot="summary"><span class="text-2xl"><strong>From the web</strong></span></svelte:fragment>
+                    <svelte:fragment slot="content">
+                        <div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+                            <div class="input-group-shim">https://</div>
+                            <input class="text-xl" type="text" placeholder="www.example.com" />
+                        </div>
+                    </svelte:fragment>
+                </AccordionItem>
+                <AccordionItem>
+                    <svelte:fragment slot="lead">(upload icon)</svelte:fragment>
+                    <svelte:fragment slot="summary"><span class="text-2xl"><strong>From a local file</strong></span></svelte:fragment>
+                    <svelte:fragment slot="content">
+                        <FileDropzone name="files">
+                            <svelte:fragment slot="message"><strong>Upload a file</strong> or drag and drop</svelte:fragment>
+                            <svelte:fragment slot="meta">.txt, .pdf, and .docx currently supported</svelte:fragment>
+                        </FileDropzone>
+                    </svelte:fragment>
+                </AccordionItem>
+                <AccordionItem>
+                    <svelte:fragment slot="lead">(paste icon)</svelte:fragment>
+                    <svelte:fragment slot="summary"><span class="text-2xl"><strong>From pasted text</strong></span></svelte:fragment>
+                    <svelte:fragment slot="content">
+                        <textarea class="textarea text-xl" placeholder="Paste full text here"></textarea>
+                    </svelte:fragment>
+                </AccordionItem>
+            </Accordion>
         </svelte:fragment>
     </AccordionItem>
-    <AccordionItem open>
-        <svelte:fragment slot="summary"><h2 class="h2">Step 2: Clean</h2></svelte:fragment>
+    <AccordionItem>
+        <svelte:fragment slot="summary"><h2 class="h2">Step 2: Configure</h2></svelte:fragment>
         <svelte:fragment slot="content">
             Content Here!
         </svelte:fragment>
     </AccordionItem>
-    <AccordionItem open>
-        <svelte:fragment slot="summary"><h2 class="h2">Step 3: Export</h2></svelte:fragment>
+    <AccordionItem>
+        <svelte:fragment slot="summary"><h2 class="h2">Step 3: Process</h2></svelte:fragment>
+        <svelte:fragment slot="content">
+            Content Here!
+        </svelte:fragment>
+    </AccordionItem>
+    <AccordionItem>
+        <svelte:fragment slot="summary"><h2 class="h2">Step 4: Export</h2></svelte:fragment>
         <svelte:fragment slot="content">
             Content Here!
         </svelte:fragment>
