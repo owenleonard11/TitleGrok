9d6e22258299b119b5d6f37e18446b14d470f3b3
Owen Leonard <owenleonard11@gmail.com>
2024-03-07 10:42:18 -0500

    Initial commit.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.postcss'
+	import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton'
+	import { LightSwitch } from '@skeletonlabs/skeleton'
+	import { page } from '$app/stores';
+</script>
+
+<TabGroup class="text-2xl px-8 pt-8">
+	<TabAnchor selected = {$page.url.pathname === "/"} href="/">
+		<svelte:fragment slot="lead">TitleGrok</svelte:fragment>
+	</TabAnchor>
+	<TabAnchor selected = {$page.url.pathname === "/usage"} href="/usage">Usage</TabAnchor>
+	<TabAnchor selected = {$page.url.pathname === "/about"} href="/about">About</TabAnchor>
+</TabGroup>
+
+<slot />
+
+
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,4 @@
+<div class="card p-4 text-xl p-8">
+    TitleGrok is a utility for retrieving and formatting lists of book titles from raw text, websites, and files. 
+    See <a href="/usage" class="anchor">Usage</a> for a quickstart guide and 
+</div>
