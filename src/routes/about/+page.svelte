4be912a502a69c448d697bb4659924993cd642f5
Owen Leonard <owenleonard11@gmail.com>
2024-03-07 12:48:19 -0500

    Finish About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,24 @@
-<div class="card p-4 text-xl p-8">
-    TitleGrok is a utility for retrieving and formatting lists of book titles from raw text, websites, and files. 
-    See <a href="/usage" class="anchor">Usage</a> for a quickstart guide and 
+<div class="card p-4 text-xl p-8 px-16">
+    <p>
+        TitleGrok is a utility for retrieving and formatting lists of book titles from raw text, websites, and files. 
+        See <a href="/usage" class="anchor">Usage</a> for a quickstart guide and 
+        <a href="https://titlegrok.readthedocs.io/en/latest/" class="anchor">ReadTheDocs</a> for more complete 
+        documentation. TitleGrok is fully open-source and licensed under the 
+        <a href="https://github.com/owenleonard11/TitleGrok/blob/master/LICENSE" class="anchor">
+            GNU General Public License v2.0
+        </a>.
+        Source code is available on 
+        <a href="https://github.com/owenleonard11/TitleGrok/" class="anchor">GitHub</a>, where you can report issues
+        and find information about contributing.
+    </p>
+    <p class="my-10">
+        This site was built using the <a href="https://svelte.dev" class="anchor">Svelte</a> JavaScript framework and the 
+        <a href="https://skeleton.dev" class="anchor">Skeleton</a> UI Toolkit. The backend uses Python 3.9 with 
+        <a href="https://fastapi.tiangolo.com/" class="anchor">FastAPI</a> to handle requests. TitleGrok is hosted,
+        regrettably, on Amazon Web Services. If you enjoy using the site, please consider supporting 
+        <a href="https://www.amazonlaborunion.org/" class="anchor">unionization efforts at Amazon</a>.
+    </p>
+    <p>
+        TitleGrok is built and maintained by me, <a href="https://github.com/owenleonard11" class="anchor">Owen Leonard</a>.
+    </p>
 </div>
