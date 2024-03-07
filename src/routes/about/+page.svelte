34110b5ac3db277e474e82f8154dd0cdc757f4ef
Owen Leonard <owenleonard11@gmail.com>
2024-03-07 15:11:31 -0500

    Update about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,6 @@
 <div class="card text-xl p-8 mx-8">
     <p>
-        TitleGrok is a utility for retrieving and formatting lists of books and articles from raw text, websites, and files. 
+        TitleGrok is a utility for retrieving and formatting lists of books and articles from websites, files, and raw text. 
         See <a href="/usage" class="anchor">Usage</a> for a quickstart guide and 
         <a href="https://titlegrok.readthedocs.io/en/latest/" class="anchor">ReadTheDocs</a> for more complete 
         documentation. TitleGrok is fully open-source and licensed under the 
